5311d9843f3fa4cae30873d8345ee3ef4121f220
Teammid <ja.natthakorn_st@tni.ac.th>
2022-01-23 19:45:00 +0700

    Interface

--- App.js
+++ App.js
@@ -1,35 +1,77 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import ModelExample from './component/12-15-2021/ModelExample'
-import AlertExample from './component/8-15-2021/AlertExample'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './Application/Home';
+import Seach from './Application/Seach';
+import Account from './Application/Account';
+import Status from './Application/Status';
+import React from 'react';
 
 
-const App = () => {
+
+const Tab = createBottomTabNavigator();
+export default  app = () => {
   return (
-   //<AlertExample/>
-   <ModelExample/>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => {
+            let iconName;
+            if (route.name == 'Home') {
+              iconName = 'home';
+              size = focused ? 25 : 20;
+            } else if (route.name == 'Seach') {
+              iconName = 'search';
+              size = focused ? 25 : 20;
+            }
+            else if (route.name == 'Status') {
+              iconName = 'book';
+              size = focused ? 25 : 20;
+            }
+            else if (route.name == 'Account') {
+              iconName = 'user';
+              size = focused ? 25 : 20;
+            }
+            return (
+              <FontAwesome5
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            )
+          }
+        })}
+        tabBarOptions={{
+          labelStyle: { fontSize: 14 },
+          activeTintColor: '#0162A8',
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{ tabBarBadge: 3 }}
+        />
+        <Tab.Screen
+          name="Seach"
+          component={Seach}
+
+        />
+        <Tab.Screen
+          name="Status"
+          component={Status}
+
+        />
+        <Tab.Screen
+          name="Account"
+          component={Account}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
-export default App
 
-/* import {View,Text,Button} from 'react-native';
-import Footer from "./component/Footer";
-import Logo from './component/Logo'
 
-const App = () =>{
-  const showData = ()=>{
-    alert('Hello World :)')
-  }
-  return (
-    <View>
-      <Logo/>
-      <Text></Text>
-      <Button title='Click' onPress={showData}/>
-      <Footer/>
-    </View>
-  )
-}
 
-export default App;
- */
+
